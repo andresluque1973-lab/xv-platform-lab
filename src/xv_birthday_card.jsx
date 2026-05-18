4f38ca9cc2145af53df5910308fb26e14b02cd88
andresluque1973-lab <andresluque1973@gmail.com>
2026-05-18 20:25:58 -0300

    Add admin routing

--- src/xv_birthday_card.jsx
+++ src/xv_birthday_card.jsx
@@ -1,1 +1,15 @@
-export { default } from './templates/standard1'
+import Standard1 from './templates/standard1.jsx';
+import AdminPage from './admin/AdminPage.jsx';
+
+function AppRouter() {
+  const slug = window.location.pathname.split('/')[1] || '';
+
+  if (slug === 'admin') {
+    return <AdminPage />;
+  }
+
+  // Futuras rutas: standard2, expirado, landing, notFound
+  return <Standard1 />;
+}
+
+export default AppRouter;
