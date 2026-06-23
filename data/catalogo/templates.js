1db794e3ee09e7c0e28b6e9b5d24be491e41bff6
andresluque1973-lab <andresluque1973@gmail.com>
2026-06-22 21:34:31 -0300

    Update templates.js
    change Proximamente x Disponible

--- data/catalogo/templates.js
+++ data/catalogo/templates.js
@@ -10,7 +10,7 @@ export const catalogo = {
   version: "1",
   templates: [
     { codigo: "S1", plan: "STANDARD", nombre: "Standard 1", estado: "disponible"   },
-    { codigo: "S2", plan: "STANDARD", nombre: "Standard 2", estado: "proximamente" },
+    { codigo: "S2", plan: "STANDARD", nombre: "Standard 2", estado: "disponible" },
     { codigo: "S3", plan: "STANDARD", nombre: "Standard 3", estado: "proximamente" },
     { codigo: "P1", plan: "PREMIUM",  nombre: "Premium 1",  estado: "proximamente" },
     { codigo: "P2", plan: "PREMIUM",  nombre: "Premium 2",  estado: "proximamente" },
